9f54ef04d4d2ce9c80360a269ae5c891f73b7caf
Coding Tiger <coding.tiger.mail@gmail.com>
2025-02-10 10:56:52 +0530

    useRef , useReducer

--- react/README.md
+++ react/README.md
@@ -2,11 +2,8 @@
 2. Components
 3. props
 4. Importing and adding CSS
-5. useState Hook
-6. useEffect Hook
-7. useParams Hook
-8. Nested Components - children
-9. Nested Routes - Outlet
-10. LocalStorage , sessionStorage , Cookies
-11. Context API (useContext Hook)
-12. useNavigate Hook
+5. Nested Components - children
+6. Nested Routes - Outlet
+7. LocalStorage , sessionStorage , Cookies
+8. Context API 
+9. useNavigate , useContext , useState , useEffect , useMemo , useCallback , memo , useParams , useRef , 
--- react/src/pages/Hooks.jsx
+++ react/src/pages/Hooks.jsx
@@ -1,16 +1,45 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Counter from "../components/Counter";
 
 const Hooks = () => {
+  const [change, setChange] = useState(false);
+  const [count, setCount] = useState(1);
+
+  // useNavigate
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate("/admin");
   };
 
+  //useMemo - memoise or cache
+  const calculate = useMemo(() => {
+    return 5 * 6;
+  }, [change]);
+
+  //useCallBack
+  const increment = useCallback(() => {
+    setCount((pre) => pre + 2);
+  }, [change]);
+
+  //useRef
+  const inputRef = useRef();
+
+  const handleUpload = () => {
+    inputRef.current.click();
+  };
+
   return (
     <>
       <h2>
-        <button onClick={handleClick}>Go to Home</button>
+        <Counter count={5} />
+        <button onClick={increment}>Go to Home</button>
+        <p>calculation : {count}</p>
+
+        <input type="file" style={{ display: "none" }} ref={inputRef} />
+
+        <button onClick={handleUpload}>Upload Profile</button>
       </h2>
     </>
   );
