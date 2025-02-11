ea1c995dbcc07ba4b97bccf8bdf6610f745c73a6
Coding Tiger <coding.tiger.mail@gmail.com>
2025-02-11 10:04:40 +0530

    core and custom module in node.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
+// fs , http , path , os
 const http = require("http");
+const os = require("os");
+
+const custom = require("./custom");
+
+console.log(`Sum : ${custom.add(5, 10)}`);
+
+// console.log("Platform : " + os.platform());
+// console.log("Total Memory : " + os.totalmem());
+// console.log("Free Memory : " + os.freemem());
 
 const server = http.createServer();
 
 server.listen(4000, () => {
   console.log("Server is listening on PORT : 4000");
 });
+
+/*
+  1. fast execution
+  2. asyncronous and non blocking
+  3. single programming Language -    JavaScript
+  4. Large community support
+*/
