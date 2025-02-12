0aee97713c11f25dc8799341b1d19f1b5919b3ac
Coding Tiger <coding.tiger.mail@gmail.com>
2025-02-12 16:44:19 +0530

    Edit & Update integration

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { toast } from "react-toastify";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const [change, setChange] = useState(true);
+  const [note, setNote] = useState("");
+  const [id, setId] = useState(null);
 
   const fetchAllNotes = async () => {
     const data = await fetch(`http://localhost:4000/api/note`);
@@ -15,33 +19,91 @@ const App = () => {
     const data = await fetch(`http://localhost:4000/api/note`, {
       method: "POST",
       body: JSON.stringify({
-        title: "From frontend",
+        title: note,
         description: "From frontend",
       }),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    const res = await data.json()
-    console.log(res);
+    const res = await data.json();
+    if (res?.ok) {
+      setChange((pre) => !pre);
+      setNote("");
+      toast.success(res?.msg);
+    }
+  };
+
+  const deleteNote = async (id) => {
+    const data = await fetch(`http://localhost:4000/api/note/${id}`, {
+      method: "DELETE",
+    });
+    const res = await data.json();
+    if (res?.ok) {
+      setChange((pre) => !pre);
+      toast.warn(res?.msg);
+    }
+  };
+
+  const handleEdit = (e) => {
+    setId(e._id);
+    setNote(e.title);
+  };
+
+  const updateNote = async () => {
+    const data = await fetch(`http://localhost:4000/api/note/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title: note,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const res = await data.json();
+    if (res?.ok) {
+      setNote("");
+      setId(null);
+      setChange((pre) => !pre);
+      toast.success(res?.msg);
+    }
   };
 
   useEffect(() => {
     fetchAllNotes();
-  }, []);
+  }, [change]);
 
   return (
     <main>
       <div className="container">
         <div className="input_box">
-          <input type="text" placeholder="Write note here..." />
-          <button onClick={addNote}>Add Note</button>
+          <input
+            type="text"
+            placeholder="Write note here..."
+            onChange={(e) => setNote(e.target.value)}
+            value={note ? note : ""}
+          />
+          <button onClick={id ? updateNote : addNote}>
+            {id ? "Update" : "Add Note"}
+          </button>
         </div>
         <div className="notes">
           <h2>All Notes</h2>
           {notes.length > 0 ? (
             notes.map((e) => {
-              return <SingleNote key={e._id} e={e} />;
+              return (
+                <div key={e._id} className="single_note">
+                  <p className="note">{e.title}</p>
+                  <p className="icons">
+                    <span onClick={() => handleEdit(e)}>
+                      <FiEdit className="edit" />
+                    </span>
+                    <span onClick={() => deleteNote(e._id)}>
+                      <MdDelete className="delete" />
+                    </span>
+                  </p>
+                </div>
+              );
             })
           ) : (
             <p>No Notes Available !</p>
@@ -53,19 +115,3 @@ const App = () => {
 };
 
 export default App;
-
-const SingleNote = ({ e }) => {
-  return (
-    <div className="single_note">
-      <p className="note">{e.title}</p>
-      <p className="icons">
-        <span>
-          <FiEdit className="edit" />
-        </span>
-        <span>
-          <MdDelete className="delete" />
-        </span>
-      </p>
-    </div>
-  );
-};
--- server/controller/note-controller.js
+++ server/controller/note-controller.js
@@ -4,7 +4,9 @@ exports.addNote = async (req, res) => {
   const { title, description } = req.body;
 
   if (!title || !description) {
-    return res.status(400).json({ msg: "Title & Description not added !" });
+    return res
+      .status(400)
+      .json({ msg: "Title & Description not added !", ok: false });
   }
 
   const note = new Note({
@@ -14,12 +16,14 @@ exports.addNote = async (req, res) => {
 
   const savedNote = await note.save();
 
-  res.status(200).json({ msg: "Added new Note !", data: savedNote });
+  res.status(200).json({ msg: "Added new Note !", data: savedNote, ok: true });
 };
 
 exports.getNotes = async (req, res) => {
   const notes = await Note.find();
-  res.status(200).json({ msg: "Notes fetched successfully !", data: notes });
+  res
+    .status(200)
+    .json({ msg: "Notes fetched successfully !", data: notes, ok: true });
 };
 
 exports.updateNote = async (req, res) => {
@@ -27,11 +31,13 @@ exports.updateNote = async (req, res) => {
   const { title, description } = req.body;
 
   if (!id) {
-    return res.status(400).json({ msg: "Id is required !" });
+    return res.status(400).json({ msg: "Id is required !", ok: false });
   }
 
   if (!title && !description) {
-    return res.status(400).json({ msg: "Title or Description is required !" });
+    return res
+      .status(400)
+      .json({ msg: "Title or Description is required !", ok: false });
   }
 
   let updates = {};
@@ -46,17 +52,19 @@ exports.updateNote = async (req, res) => {
     { new: true }
   );
 
-  res.status(201).json({ msg: "Note is updated !", data: updatedNote });
+  res
+    .status(201)
+    .json({ msg: "Note is updated !", data: updatedNote, ok: true });
 };
 
 exports.deleteNote = async (req, res) => {
   const { id } = req.params;
 
   if (!id) {
-    return res.status(400).json({ msg: "Id is required !" });
+    return res.status(400).json({ msg: "Id is required !", ok: false });
   }
 
   await Note.findByIdAndDelete(id);
 
-  res.status(200).json({ msg: "Note is deleted !" });
+  res.status(200).json({ msg: "Note is deleted !", ok: true });
 };
